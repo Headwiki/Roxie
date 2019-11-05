bcd0e909841e8f0e6d23b6d110a6e345179d9658
Henrik Hedlund Andersen <heheand@gmail.com>
2019-11-05 18:18:47 +0100

    OOP sketch

--- src/main.rs
+++ src/main.rs
@@ -2,111 +2,120 @@
 extern crate clap;
 use clap::App;
 
-use std::net::{TcpListener, TcpStream, SocketAddr};
+use std::io::{Read, Write};
+use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
-use std::io::{Read, Write};
 
-struct connection_info {
+struct Connection {
     source: SocketAddr,
-    destination: SocketAddr
+    destination: SocketAddr,
+    //listener: Option<TcpListener>, // Source stream
+    //stream: Option<TcpStream>,     // Destination stream
+}
+
+impl Connection {
+    fn new(source: SocketAddr, destination: SocketAddr) -> Connection {
+        Connection {
+            source: source,
+            destination: destination,
+        }
+    }
+
+    fn run(&mut self) -> Result<(), std::io::Error> {
+        let listener = TcpListener::bind(self.source)?;
+        println!("Listening on {source}", source = self.source);
+
+        for source_stream in listener.incoming() {
+            match source_stream {
+                Ok(source_stream) => {
+                    self.forward_data(source_stream).unwrap();
+                }
+                Err(_) => {
+                    println!("Error");
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn forward_data(&self, mut s_stream: TcpStream) -> std::io::Result<()> {
+        let mut buffer = [0u8; 256];
+
+        while match s_stream.read(&mut buffer) {
+            Ok(size) => {
+                if size != 0 {
+                    println!("Received from source: {:?}", &buffer[0..size]);
+                    let mut d_stream = TcpStream::connect(self.destination)?;
+                    d_stream.write(&buffer[0..size]).unwrap();
+                    println!("Sent buffer, awaiting reply...");
+
+                    let mut buffer_repsonse = [0u8; 256];
+                    match d_stream.read(&mut buffer_repsonse) {
+                        Ok(_) => {
+                            println!("Received from destination: {:?}", &buffer_repsonse[0..size]);
+                            s_stream.write(&buffer_repsonse[0..size]).unwrap();
+                        }
+                        Err(e) => {
+                            println!("Failed to receive data: {}", e);
+                        }
+                    }
+                }
+                true
+            }
+            Err(error) => panic!("{}", error),
+        } {}
+
+        Ok(())
+    }
+
 }
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let _matches = App::from_yaml(yaml).get_matches();
 
-    thread::spawn(move || {
-        source_to_roxie("127.0.0.1:3003".parse().unwrap()).unwrap(); 
-    });
-    thread::spawn(move || {
-        //let _stream = TcpStream::connect("127.0.0.1:3003").unwrap();
-        client_test();
+    let mut new_connection = Connection::new(
+        "127.0.0.1:3003".parse().unwrap(), 
+        "127.0.0.1:3003".parse().unwrap()
+    );
 
-        
+    thread::spawn(move || {
+        new_connection.run().unwrap();
     });
-    loop{
+
+    thread::sleep(Duration::from_secs(1));
+    client_test();
+
+    loop {
         thread::sleep(Duration::from_secs(1));
     }
 }
 
 
-fn connection (info: &connection_info) -> std::io::Result<()>{
-
-    // Create stream to destination
-
-
-    // Create listener for source
-
-    Ok(())
-}
-
-fn source_to_roxie(socket: SocketAddr) -> std::io::Result<()> {
-    let listener = TcpListener::bind(socket)?;
-    println!("Listening on {socket}", socket=socket);
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(move || {
-                    roxie_to_destination(stream).unwrap();
-                });
-            }
-            Err(_) => {
-                println!("Error");
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn roxie_to_destination(mut stream: TcpStream) -> std::io::Result<()> {
-
-    //let mut stream = TcpStream::connect(socket)?;
-    let mut buffer = [0u8;64];
-
-        while match stream.read(&mut buffer) {
-            Ok(size) => { 
-                if size != 0 {
-                    println!("{:?}", &buffer[0..size]);
-                    stream.write(&buffer[0..size]).unwrap();
-                }
-                true
-             },
-            Err(error) => {
-                panic!("{}", error)
-            }
-        } {}
-
-    Ok(())
-}
-
 fn client_test() {
     match TcpStream::connect("127.0.0.1:3003") {
         Ok(mut stream) => {
-            println!("Successfully connected to server in port 3003");
+            println!("Test: Successfully connected to server in port 3003");
 
             let msg = b"Hello!";
 
             stream.write(msg).unwrap();
-            println!("Sent Hello, awaiting reply...");
+            println!("Test: Sent Hello");
 
-            let mut data = [0 as u8; 6]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == msg {
-                        println!("Reply is ok!");
-                    } else {
-                        let text = std::str::from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text);
-                    }
+            let mut buffer = [0u8;256];
+
+            match stream.read(&mut buffer) {
+                Ok(size) => {
+                    println!("Test: Got: {:?}", &buffer[0..size]);
                 },
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
+                Err(_) => {
+
                 }
             }
-        },
+
+        }
         Err(e) => {
             println!("Failed to connect: {}", e);
         }
